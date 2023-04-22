2fb43ec5772a5e5a9ae04d11367a63b96f418daa
Ramon Arellano <ramon.arellano@rarellano-soft.com>
2023-04-21 21:43:03 -0600

    Change from postgres db to mysql db

--- src/conf/database.config.js
+++ src/conf/database.config.js
@@ -1,14 +1,31 @@
 require('dotenv').config()
 const  { Sequelize } = require('sequelize');
 
-const dbDatabase = process.env.DB_DATABASE
-const dbUsername = process.env.DB_USERNAME
-const dbPassword = process.env.DB_PASSWORD
-const dbHost = process.env.DB_HOST
-const dbDialect = process.env.DB_DIALECT
+// const dbDatabasePos = process.env.DB_DATABASE_POS
+// const dbUsernamePos = process.env.DB_USERNAME_POS
+// const dbPasswordPos = process.env.DB_PASSWORD_POS
+// const dbHostPos = process.env.DB_HOST_POS
+// const dbPortPos = process.env.DB_PORT_POS
+
+const dbDatabaseMys = process.env.DB_DATABASE_MYS
+const dbUsernameMys = process.env.DB_USERNAME_MYS
+const dbPasswordMys = process.env.DB_PASSWORD_MYS
+const dbHostMys = process.env.DB_HOST_MSY
+const dbDialectMys = process.env.DB_DIALECT_MYS
+const dbPortMys = process.env.DB_PORT_MYS
+
+// const sequelizePostgres = new Sequelize(
+//     `postgres://${dbUsernamePos}:${dbPasswordPos}@${dbHostPos}:${}/${dbDatabasePos}`
+// );
 
 const sequelize = new Sequelize(
-    `postgres://${dbUsername}:${dbPassword}@${dbHost}:5432/${dbDatabase}`
+    dbDatabaseMys,
+    dbUsernameMys,
+    dbPasswordMys, {
+        host: dbHostMys,
+        port:dbPortMys,
+        dialect: dbDialectMys
+    }
 );
 
 module.exports = sequelize;
